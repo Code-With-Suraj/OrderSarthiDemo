0d2e3f298bcf8dab1ce1778b1656dca4ea363bcb
suraj singh <surajsingh.noida98@gmail.com>
2026-08-20 21:02:14 +0530

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -5,7 +5,7 @@
 
 const CONFIG = {
   // Replace this with your deployed Google Apps Script Web App URL
-  API_BASE_URL: "https://script.google.com/macros/s/AKfycby6nSSNIp5l48-lerbJUMQJ8IZiL9MR4ZfcwIIsf-321mRQ-B_W4YntEgc_GnSDMIVV/exec",
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbxMNLoP2N5aMPGmfIx864BgQlG5nQAdk4YXFG3F5H18ZrsKj1yyXl43ER0IMxF6reRt8Q/exec",
 
   // App & White-Label Platform Settings
   DEFAULT_SHOP_NAME: "Local Express Shop",
